ce3038bd7acedb4180606af201112b346a2232f0
manuel.carrasco.m <manuel.carrasco.m@1863f854-1fd2-11df-a719-01f031a10480>
2010-02-22 18:55:17 +0000

    first commit
    git-svn-id: https://shell2junit.googlecode.com/svn/trunk@2 1863f854-1fd2-11df-a719-01f031a10480

---
+++ sh2ju_example.sh
@@ -0,0 +1,28 @@
+#/bin/sh
+
+#### Include the library
+. `dirname $0`/sh2ju.sh
+
+#### Clean old reports
+juLogClean
+
+#### Success command
+juLog  -name=myTrueCommand                  true
+
+#### Failure
+juLog  -name=myFalseCommand                 false
+
+#### The test fails because the word 'world' is found in command output
+juLog  -name=myErrorCommand -ierror=world   echo Hello World
+
+#### A sql command
+juLog  -name=mySqlCommand                   psql -h localhost -U postgres -c 'select now()'
+
+#### A call to a customized method
+myCmd() {
+   ls -l $*
+   return 0
+}
+
+juLog  -name=myCustomizedMethod             myCmd '*.sh'
+
